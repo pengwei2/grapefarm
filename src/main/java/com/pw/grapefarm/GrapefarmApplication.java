4b14453388d787af877aabd90743e53adc0d6539
pw <yodang2008@gmail.com>
2019-03-25 22:40:38 +0800

    'init'

---
+++ src/main/java/com/pw/grapefarm/GrapefarmApplication.java
@@ -0,0 +1,13 @@
+package com.pw.grapefarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapefarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrapefarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pw/grapefarm/GrapefarmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pw.grapefarm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrapefarmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
