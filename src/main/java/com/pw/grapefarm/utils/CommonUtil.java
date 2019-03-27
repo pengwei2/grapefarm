2920f4e3f8064183c087239b56f4af37227a2893
pw <yodang2008@gmail.com>
2019-03-27 17:57:53 +0800

    增加工具类，添加生成验证码方法

---
+++ src/main/java/com/pw/grapefarm/utils/CommonUtil.java
@@ -0,0 +1,19 @@
+package com.pw.grapefarm.utils;
+
+import java.util.Random;
+
+public class CommonUtil {
+    public static String genRandomStr(int randomLength){
+        String code = "";
+
+        char[] chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789".toCharArray();
+        int length = chars.length;
+        Random random = new Random();
+
+        for (int i = 0; i < randomLength ; i++) {
+            code += chars[random.nextInt(length-1)];
+        }
+
+        return code;
+    }
+}
