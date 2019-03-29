2f1d327373919fccdfc061e5f7133a57d05e52ca
pw <yodang2008@gmail.com>
2019-03-29 19:51:12 +0800

    对创建的用户进行Bcrypt密码加密

--- src/main/java/com/pw/grapefarm/GrapefarmApplication.java
+++ src/main/java/com/pw/grapefarm/GrapefarmApplication.java
@@ -3,8 +3,10 @@ package com.pw.grapefarm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class GrapefarmApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/pw/grapefarm/controllers/UserController.java
+++ src/main/java/com/pw/grapefarm/controllers/UserController.java
@@ -5,13 +5,16 @@ import com.pw.grapefarm.commons.Response;
 import com.pw.grapefarm.daos.UserDao;
 import com.pw.grapefarm.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.Date;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -19,15 +22,25 @@ public class UserController extends BaseController{
     @Autowired
     UserDao userDao;
 
+    @Resource
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @ParamValid
     @PostMapping
     public Response<String> createUser(@Valid @RequestBody User user, BindingResult bindingResult){
         String email = user.getEmail();
 
-        // 如果该邮件对应的用户已存在，则发送获取验证码失败
+        // 如果该邮件对应的用户已存在，则创建用户失败
         if(userDao.findByEmail(email) != null){
-            return new Response<>(StatusCode.fail.ordinal(),"失败","该邮箱已经注册！");
+            return new Response<>(StatusCode.fail.ordinal(),"失败","该邮箱对应的用户已存在！");
         }
+
+        // TODO 将业务代码移至service层去
+        String password  =  user.getPassword();
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        userDao.save(user);
 
         return new Response<>(StatusCode.success.ordinal(),"成功","");
     }
