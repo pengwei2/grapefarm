c12eb3e16c75ac5183e2263832291b1af8d81f02
wuyoujian <wuyoujian0313@qq.com>
2020-02-08 20:47:56 +0800

    Update PaymentController.java
    update

--- src/main/java/com/pw/grapefarm/controller/PaymentController.java
+++ src/main/java/com/pw/grapefarm/controller/PaymentController.java
@@ -16,6 +16,7 @@ import com.pw.grapefarm.service.VIPService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
